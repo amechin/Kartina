33c85be556c81df0b91217a1c54ce12bc4a97f86
sakho51 <msakho@hotmail.fr>
2019-10-22 15:38:09 +0200

    creation formulaire Aide

---
+++ KartinaProjet/KartinaProjet/Controllers/UtilisateurController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using KartinaProjet;
+
+namespace KartinaProjet.Controllers
+{
+    public class UtilisateurController : Controller
+    {
+        private KartinaProjetEntities1 db = new KartinaProjetEntities1();
+
+        // GET: Utilisateur
+        public ActionResult Index()
+        {
+            return View(db.Utilisateur.ToList());
+        }
+
+        // GET: Utilisateur/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Utilisateur utilisateur = db.Utilisateur.Find(id);
+            if (utilisateur == null)
+            {
+                return HttpNotFound();
+            }
+            return View(utilisateur);
+        }
+
+        // GET: Utilisateur/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Utilisateur/Create
+        // Afin de déjouer les attaques par sur-validation, activez les propriétés spécifiques que vous voulez lier. Pour 
+        // plus de détails, voir  https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "IdUtilisateur,Civilite,Prenom,Nom,Email,Telephone,MotDePasse,Vendeur,Bloque")] Utilisateur utilisateur)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Utilisateur.Add(utilisateur);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(utilisateur);
+        }
+
+        // GET: Utilisateur/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Utilisateur utilisateur = db.Utilisateur.Find(id);
+            if (utilisateur == null)
+            {
+                return HttpNotFound();
+            }
+            return View(utilisateur);
+        }
+
+        // POST: Utilisateur/Edit/5
+        // Afin de déjouer les attaques par sur-validation, activez les propriétés spécifiques que vous voulez lier. Pour 
+        // plus de détails, voir  https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "IdUtilisateur,Civilite,Prenom,Nom,Email,Telephone,MotDePasse,Vendeur,Bloque")] Utilisateur utilisateur)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(utilisateur).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(utilisateur);
+        }
+
+        // GET: Utilisateur/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Utilisateur utilisateur = db.Utilisateur.Find(id);
+            if (utilisateur == null)
+            {
+                return HttpNotFound();
+            }
+            return View(utilisateur);
+        }
+
+        // POST: Utilisateur/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Utilisateur utilisateur = db.Utilisateur.Find(id);
+            db.Utilisateur.Remove(utilisateur);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
