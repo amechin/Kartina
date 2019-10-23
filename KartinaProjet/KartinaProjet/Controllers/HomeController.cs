fd2eb89ab97d7b83dd8cb95d183b4865860899ef
Adrien <adrien.mechin@gmail.com>
2019-10-23 11:57:51 +0200

    Mise en place de la connexion Entity + HomeController + HomeViewModel

--- KartinaProjet/KartinaProjet/Controllers/HomeController.cs
+++ KartinaProjet/KartinaProjet/Controllers/HomeController.cs
@@ -8,21 +8,9 @@ namespace KartinaProjet.Controllers
 {
     public class HomeController : Controller
     {
+        private KartinaProjetEntities db = new KartinaProjetEntities();
         public ActionResult Index()
         {
-            return View();
-        }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
 
             return View();
         }
